f963b6d3859c2ec011c865857dff1111148e111c
Francis Chuang <francis.chuang@gmail.com>
2014-01-22 19:39:30 +1100

    Try echoing the result.

--- InlineStyle/Tests/InlineStyleTest.php
+++ InlineStyle/Tests/InlineStyleTest.php
@@ -124,9 +124,29 @@ class InlineStyleTest extends \PHPUnit_Framework_TestCase
     }
     
     public function testActivePseudoSelectors(){
+    	$html = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<style type="text/css">
+
+a:active {
+    font-size: 11px;
+}
+
+</style>
+
+</head>
+<body>
+    <a href="google.com">blah</a>
+</body>
+</html>';
     	
-    	$object = new InlineStyle($this->basedir."/testActivePseudoSelector.html");
+    	$object = new InlineStyle($html);
     	$object->applyStylesheet($object->extractStylesheets());
+    	
+    	$html = $object->getHTML();
+    	echo $html;
     }
 
     function testSortingParsedStylesheet()
