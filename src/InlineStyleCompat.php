2a38f62f37cae519eb2d64d097baa3fd1987b8f7
Christiaan <anotherhero@gmail.com>
2015-08-16 16:43:50 +0200

    Implement old testcases

---
+++ src/InlineStyleCompat.php
@@ -0,0 +1,61 @@
+<?php
+namespace InlineStyle;
+
+use InlineStyle\Css\OrderedStyleSheet;
+use InlineStyle\Html\ApplyStyleSheets;
+use InlineStyle\Html\Document;
+use InlineStyle\Html\ExtractStyleSheets;
+
+/**
+ * InlineStyleCompat
+ * @author Christiaan Baartse <anotherhero@gmail.com>
+ */
+final class InlineStyleCompat
+{
+    /** @var Document */
+    private $document;
+
+    public function __construct($html)
+    {
+        if (strlen($html) <= PHP_MAXPATHLEN && file_exists($html)) {
+            $html = file_get_contents($html);
+        }
+        $this->document = new Document($html);
+    }
+
+    public function getHTML()
+    {
+        return (string) $this->document;
+    }
+
+    public function applyStyleSheet($string)
+    {
+        $stylesheet = OrderedStyleSheet::fromString($string);
+        $applyStylesheets = new ApplyStyleSheets(
+            array($stylesheet)
+        );
+
+        $this->document = $this->document->applyTransform($applyStylesheets);
+
+        return $this;
+    }
+
+    public function applyRule($selector, $declaration)
+    {
+        $this->applyStyleSheet($selector . '{' . $declaration . '}');
+
+        return $this;
+    }
+
+    public function extractStylesheets(Document $document = null, $basedir, $devices = array('all', 'screen', 'handheld'))
+    {
+        $extractStyleSheets = new ExtractStyleSheets(
+            $basedir,
+            $devices
+        );
+
+        $this->document = $this->document->applyTransform($extractStyleSheets);
+
+        return array_map('strval', $extractStyleSheets->getStyleSheets());
+    }
+}
