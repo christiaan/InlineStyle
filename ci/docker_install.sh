57dad05c5443b4e2d8f3c65c1c54a3607c15e084
Christiaan Baartse <anotherhero@gmail.com>
2016-07-08 17:08:07 +0200

    Use phpunit configured in composer.json

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -8,7 +8,3 @@ set -xe
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
 apt-get install git unzip -yqq
-
-# Install phpunit, the tool that we will use for testing
-curl -L -o /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
-chmod +x /usr/local/bin/phpunit
